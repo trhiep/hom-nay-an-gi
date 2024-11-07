048b4a6cdcb36ac0d3a2bf8e74eb0a48df835acc
trhiep <hieptran.pa@gmail.com>
2024-11-08 01:18:04 +0700

    feat: add jwt helper to get username and userid

---
+++ HomNayAnGi/HomNayAnGiApp/Models/DTO/RecipeCategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HomNayAnGiApp.Models.DTO
+{
+    public class RecipeCategoryDTO
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public int? CreatedBy { get; set; }
+    }
+}
