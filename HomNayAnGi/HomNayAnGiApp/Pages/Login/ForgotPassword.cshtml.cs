0a2c29735ea92de685e8b3b1c3b0ea60a96f170a
Nguyen Minh Dai <nguyenminhdai203@gmail.com>
2024-11-08 23:34:19 +0700

    List usẻ

--- HomNayAnGi/HomNayAnGiApp/Pages/Login/ForgotPassword.cshtml.cs
+++ HomNayAnGi/HomNayAnGiApp/Pages/Login/ForgotPassword.cshtml.cs
@@ -14,24 +14,5 @@ namespace HomNayAnGiApp.Pages.Login
 
         }
 
-        public IActionResult OnPost(string username)
-        {
-            try
-            {
-                HttpClient _httpClient = new HttpClient();
-                HttpResponseMessage response = _httpClient.GetAsync("http://localhost:5000/api/Users/get-by-username/" + username).Result;
-                var user = response.Content.ReadFromJsonAsync<User>().Result;
-                if (response.IsSuccessStatusCode)
-                {
-                        HttpContext.Session.SetString("SignupRequest", JsonConvert.SerializeObject(signupRequestDto));
-                        return RedirectToPage("/Signup/VerifyOTP");
-                }
-                return Page();
-            }catch(Exception e)
-            {
-                return Page();
-            }
-            
-        }
     }
 }
---
+++ HomNayAnGi/HomNayAnGiApp/Pages/UsersPage/UpdatePass.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomNayAnGiApp.Pages.UsersPage
+{
+    public class UpdatePassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
